81943b422f1aa7027af55f07aac73a1bf859f227
AnaHrnndz <ana.hernandez.plaza@gmail.com>
2022-01-10 14:28:39 +0100

    lca_node_name faster with ncbi

--- newick2pickle.py
+++ newick2pickle.py
@@ -6,12 +6,10 @@ import os
 
 
 t = PhyloTree(sys.argv[1]) 
+path_out = sys.argv[2]
 name = os.path.basename(sys.argv[1])
 name_no_extension = name.replace('.faa.aln.nw', '')
-name_out = name_no_extension+'.pickle'
+name_out = path_out+name_no_extension+'.pickle'
 
 with open(name_out, "wb") as handle:
     dump(t, handle)
-
-# with open("tree.pickle", "rb") as handle:
-    # t = load(handle)
